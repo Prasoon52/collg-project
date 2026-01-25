07df1a5b6ff60506ac409f0f82bfb8726b946f34
abhas20 <darklegends2971@gmail.com>
2026-01-25 16:14:06 +0530

    removed

--- stem1/stem_backend/routes/init.routes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { initData } from "../controllers/init.controller.js";
-
-const router = express.Router();
-router.post("/init-data", initData);
-
-export default router;
